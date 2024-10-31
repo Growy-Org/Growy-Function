9afe414e091900021d826f9264b2a73819290ba3
jackytsheng <jackytsheng@gmail.com>
2024-10-31 23:37:26 +1100

    Use actual Repo in the test

--- FamilyMerchandise.Tests/DBTestHelper.cs
+++ FamilyMerchandise.Tests/DBTestHelper.cs
@@ -1,68 +1,47 @@
 using Dapper;
 using FamilyMerchandise.Function.Entities;
 using Bogus;
+using Bogus.DataSets;
+using FamilyMerchandise.Function.Models;
+using FamilyMerchandise.Function.Repositories;
 
 namespace FamilyMerchandise.Tests;
 
-public class FamilyMerchandiseDBHelper : IClassFixture<FunctionTestFixture>
+public class FamilyMerchandiseDbHelper(FunctionTestFixture fixture) : IClassFixture<FunctionTestFixture>
 {
-    private readonly FunctionTestFixture _fixture;
-    private readonly Faker _faker;
-    private const string HOME_TABLE = "inventory.homes";
-    private const string CHILDREN_TABLE = "inventory.children";
-    private const string PARENTS_TABLE = "inventory.parents";
-
-    public FamilyMerchandiseDBHelper(FunctionTestFixture fixture)
-    {
-        _faker = new Faker();
-        _fixture = fixture;
-    }
+    private readonly Faker _faker = new();
 
     [Fact]
-    public async Task InsertChild()
+    public async Task InsertInitialData()
     {
-        using var con = _fixture.ConnectionFactory.GetFamilyMerchandiseDBConnection();
-        var child = new ChildEntity
-        {
-            Name = _faker.Name.FullName(),
-            IconCode = _faker.Random.Int(0, 100),
-            DOB = _faker.Date.Past(18),
-            Gender = _faker.PickRandom("BOY", "GIRL"),
-            PointsEarned = _faker.Random.Int(0, 9999),
-        };
-        var query =
-            $"INSERT INTO {CHILDREN_TABLE} (Name, IconCode, DOB, Gender, PointsEarned) VALUES (@Name, @IconCode, @DOB, @Gender, @PointsEarned) RETURNING Id";
-        await con.ExecuteScalarAsync<Guid>(query, child);
-    }
+        var homeRepo = new HomeRepository(fixture.ConnectionFactory);
+        var childRepo = new ChildRepository(fixture.ConnectionFactory);
+        var parentRepo = new ParentRepository(fixture.ConnectionFactory);
 
-    [Fact]
-    public async Task InsertHome()
-    {
-        using var con = _fixture.ConnectionFactory.GetFamilyMerchandiseDBConnection();
-        var home = new HomeEntity()
+        var home = new Home()
         {
             Name = _faker.Address.FullAddress(),
         };
+        var homeId = await homeRepo.InsertHome(home);
 
-        var query =
-            $"INSERT INTO {HOME_TABLE} (Name) VALUES (@Name) RETURNING Id";
-        await con.ExecuteScalarAsync<Guid>(query, home);
-    }
-
-    [Fact]
-    public async Task InsertParent()
-    {
-        using var con = _fixture.ConnectionFactory.GetFamilyMerchandiseDBConnection();
-        var parent = new ParentEntity
+        var child = new Child()
         {
             Name = _faker.Name.FullName(),
             IconCode = _faker.Random.Int(0, 100),
-            HomeId = Guid.Parse("6271aec4-610a-4342-804f-c180904e735a"),
             DOB = _faker.Date.Past(18),
-            Role = _faker.Random.Int(0, 1),
+            Gender = _faker.PickRandom(ChildGender.BOY, ChildGender.GIRL),
+            PointsEarned = _faker.Random.Int(0, 9999),
         };
-        var query =
-            $"INSERT INTO {PARENTS_TABLE} (Name, HomeId, IconCode, DOB, Role) VALUES (@Name, @HomeId, @IconCode, @DOB, @Role) RETURNING Id";
-        await con.ExecuteScalarAsync<Guid>(query, parent);
+        await childRepo.InsertChild(homeId, child);
+
+        var parent = new Parent
+        {
+            Name = _faker.Name.FullName(),
+            IconCode = _faker.Random.Int(0, 100),
+            DOB = _faker.Date.Past(18),
+            Role = _faker.PickRandom(ParentRole.FATHER, ParentRole.MOTHER),
+        };
+
+        await parentRepo.InsertParent(homeId, parent);
     }
 }
