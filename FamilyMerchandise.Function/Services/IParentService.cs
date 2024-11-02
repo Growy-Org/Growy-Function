2e549fe708a9c914f8f96c211d233ecb7ad2b523
jackytsheng <jackytsheng@gmail.com>
2024-11-02 17:30:12 +1100

    Adjust interface

--- FamilyMerchandise.Function/Services/IParentService.cs
+++ FamilyMerchandise.Function/Services/IParentService.cs
@@ -7,24 +7,25 @@ public interface IParentService
 {
     // Assignments
     public Task<List<Assignment>> GetAllAssignmentsByHomeId(Guid homeId);
-    public Assignment GetAssignment(Guid assignmentId);
+    // TODO: Refactor this assignment method into it's own service
+    public Task<Assignment> GetAssignment(Guid assignmentId);
     public Task<Guid> CreateAssignment(CreateAssignmentRequest request);
-    public void EditAssignment(Guid assignmentId, Assignment assignment);
-    public void CompleteAssignment(Guid assignmentId);
-    public Assignment CreateStepToAssignment(Guid assignmentId);
-    public void EditStep(Guid stepId);
+    public Task EditAssignment(Guid assignmentId, Assignment assignment);
+    public Task CompleteAssignment(Guid assignmentId);
+    public Task<Guid> CreateStepToAssignment(Guid assignmentId);
+    public Task EditStep(Guid stepId);
 
     // Wishes
-    public List<Wish> GetWishesByHomeId(Guid homeId);
-    public Wish EditWishCost(Guid wishId);
+    public Task<List<Wish>> GetAllWishesByHomeId(Guid homeId);
+    public Task EditWishCost(Guid wishId);
 
     // Achievements
-    public List<Assignment> GetAllAchievementByHomeId(Guid homeId);
+    public Task<List<Assignment>> GetAllAchievementByHomeId(Guid homeId);
     public Task<Guid> CreateAchievement(CreateAchievementRequest request);
-    public void EditAchievement(Guid achievementId);
-    public void GrantAchievementBonus(Guid achievementId);
+    public Task EditAchievement(Guid achievementId);
+    public Task GrantAchievementBonus(Guid achievementId);
 
     // Penalties
-    public List<Penalty> GetAllPenaltiesByHomeId(Guid homeId);
+    public Task<List<Penalty>> GetAllPenaltiesByHomeId(Guid homeId);
     public Task<Guid> CreatePenalty(CreatePenaltyRequest request);
 }
